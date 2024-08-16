5f31a7b519b655cb49bf62980638616b4f054527
TODYAS <bogdansytin@gmail.com>
2024-08-16 15:42:38 +0300

    Настроили urls и views. Попрактиковал работу с re_path.

--- CheeseDjango/urls.py
+++ CheeseDjango/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from firstapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    re_path(r'^products/(?P<productid>\d+)/', views.products),
+    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users),
+    re_path(r'^phone/(?P<variable_name>\d+)/', views.phone),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -5,3 +5,20 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("<h2>Главная</h2>")
+
+def about(request):
+    return HttpResponse("<h2>О нас</h2>")
+
+def contact(request):
+    return HttpResponse("<h2>О нас</h2>")
+
+def products(request, productid):
+    output = "<h2>Продукт № {0}</h2>".format(productid)
+    return HttpResponse(output)
+def users(request, id, name):
+    output = "<h2>Пользовательн</h2><h3>id = {0}, имя - {1}</h3>".format(id, name)
+    return HttpResponse(output)
+
+def phone(request, variable_name):
+   output = "<h2>You're phone number - {0}</h2>".format(variable_name)
+   return HttpResponse(output)
